460c7f4b5fc138c03d941951fa02ae46833a2b86
Victor <u21221311@utp.edu.pe>
2025-04-03 14:56:34 -0500

    Primer Commit

---
+++ src/test/java/com/vet/CliniVet/CliniVetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet.CliniVet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
