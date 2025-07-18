b0ed72536d463e31903c9bfe5116c986e347a979
VICTOR ANDREE SEVILLANO VEGA <U21221311@utp.edu.pe>
2025-07-17 20:59:30 -0500

    Se soluciono un bug que no permitia arrancar el problema

--- src/main/java/webvet/v1/infraestructure/controllers/UsuarioController.java
+++ src/main/java/webvet/v1/infraestructure/controllers/UsuarioController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/admin/authentication/")
+@RequestMapping("/api/v1/authentication/")
 @Tag(name = "Usuarios", description = "API para creacion de Usuarios")
 public class UsuarioController {
 
